09fc4b84d44123f909a0ecc6060d48d3b9cc6070
ADK <adk.kda10@gmail.com>
2023-12-18 09:29:06 +0000

    Updated models

--- Controller/registerpatients.controller.js
+++ Controller/registerpatients.controller.js
@@ -1,103 +1,11 @@
-const registerpatients = require("../model/registerpatient.model");
+const Patient = require("../models/patient");
 
-// Create a Registerpatients
-const createRegisterpatients = async (req, res) => {
-  const { PatientID, name, gender, phonenumber, contact } = req.body;
-
-  console.log("PatientID ", PatientID, " name", name);
-
+exports.registerPatient = async (req, res) => {
   try {
-    const registerpatients = await registerpatients.create({
-      PatientID,
-      name,
-      gender,
-      phonenumber,
-      contact
-    });
-
-    return res.status(200).json({
-      registerpatients,
-    });
+    const patient = new Patient(req.body);
+    await patient.save();
+    res.status(201).json(patient);
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-    });
+    res.status(500).json({ error: error.message });
   }
-};
-
-// Update a registerpatients by ID
-const updateregisterpatients = async (req, res) => {
-  const { id } = req.params;
-  const { PatientID, name, gender, phonenumber, contact } = req.body;
-
-  try {
-    const updatedregisterpatients = await registerpatients.findByIdAndUpdate(
-      id,
-      { PatientID, name, gender, phonenumber, contact },
-      { new: true }
-    );
-
-    if (!updatedTodo) {
-      return res.status(404).json({
-        error: "registerpatients not found",
-      });
-    }
-
-    return res.status(200).json({
-      todo: updatedTodo,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-    });
-  }
-};
-
-// Get all registerpatients
-const getAllregisterpatients = async (req, res) => {
-  try {
-    const registerpatients = await Todo.find();
-
-    return res.status(200).json({
-      todos,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-    });
-  }
-};
-
-// Delete a registerpatients by ID
-const deleteregisterpatients = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const deletedregisterpatients = await registerpatients.findByIdAndDelete(id);
-
-    if (!deletedregisterpatients) {
-      return res.status(404).json({
-        error: "registerpatients not found",
-      });
-    }
-
-    return res.status(200).json({
-      message: "registerpatients deleted successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: "Internal Server Error",
-    });
-  }
-};
-
-module.exports = {
-  createRegisterpatients,
-  updateRegisterpatients,
-  getAllRegisterpatients,
-  deleteRegisterpatients,
 };
